71cd4985b2a32459dbc676ff637285732f4970da
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-25 23:31:53 -0300

    Configurando o ambiente

---
+++ src/main/java/br/com/ialmeida/cryptographyspringboot/CryptographySpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.cryptographyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptographySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptographySpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ialmeida/cryptographyspringboot/CryptographySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.cryptographyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptographySpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
